14f9ab85256d2006831012630260b6bbcf97f0a3
Ajay26krishna <ajay.dak.26@gmail.com>
2022-03-16 09:37:50 +0530

    2nd

---
+++ intro.js
@@ -0,0 +1,13 @@
+function sample(){
+    console.log("Sample function");
+}
+sample();
+let s=function()
+{
+    console.log("s function");
+}
+s();
+let square=function(num){
+    return num*num;
+}
+console.log(square(5));
